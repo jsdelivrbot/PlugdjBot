0a4eb3ca74fca7efa4a05db057e33adced1f3668
natzki <mmaste2588@gmail.com>
2014-10-14 00:36:49 +0200

    Update Bot3.js

--- Bot3.js
+++ Bot3.js
@@ -4,7 +4,7 @@ var bot = new PlugAPI({
     "email": "",
     "password": ""
 });
-bot.connect('mcorigins-network'); // The part after https://plug.dj
+bot.connect('/mcorigins-network'); // The part after https://plug.dj
 
 bot.on('roomJoin', function(room) {
     console.log("Joined " + room);
