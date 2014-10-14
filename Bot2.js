cc6b5016ec65dfd20b4ca6226401e480e907eb55
natzki <mmaste2588@gmail.com>
2014-10-14 18:31:58 +0200

    Update Bot2.js

--- Bot2.js
+++ Bot2.js
@@ -1,10 +1,10 @@
 var PlugAPI = require('plugapi');
 
 var bot = new PlugAPI({
-    "email": "",
-    "password": ""
+    "email": "natzkiPlaysMinecraft@gmail.com",
+    "password": "Minecraft-hg"
 });
-bot.connect('/mcorigins-network'); // The part after https://plug.dj
+bot.connect('mcorigins-network'); // The part after https://plug.dj
 
 bot.on('roomJoin', function(room) {
     console.log("Joined " + room);
