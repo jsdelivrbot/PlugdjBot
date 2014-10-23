ac386c88f5ef0d2191154ce002b6d1e38b99ccd3
natzki <mmaste2588@gmail.com>
2014-10-23 20:59:40 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -4,7 +4,7 @@ var bot = new PlugAPI({
     "email": "natzkiPlaysMinecraft@gmail.com",
     "password": "Minecraft-hg"
 });
-bot.connect('/mcorigins-network'); // The part after https://plug.dj
+bot.connect('/mineplex'); // The part after https://plug.dj
 
 bot.on('roomJoin', function(room) {
     console.log("Joined " + room);
